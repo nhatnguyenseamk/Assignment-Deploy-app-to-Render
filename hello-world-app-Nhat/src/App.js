a78ba9a4b5b0b7447d83b334d991921c2095167d
nhatnguyenseamk <nhat.nguyen@seamk.fi>
2025-11-17 16:39:07 +0200

    Initial commit

---
+++ hello-world-app-Nhat/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh", 
+      fontSize: "2rem" 
+    }}>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
